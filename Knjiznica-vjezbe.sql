eedc6ff4377798664fbbf174178ff8f6706d75ec
Josip-github <josipcota1@gmail.com>
2021-05-19 15:31:38 +0200

    select, between
    igranje s tablicom katalog da vidim kako izgleda

--- Knjiznica-vjezbe.sql
+++ Knjiznica-vjezbe.sql
@@ -1,3 +1,6 @@
 select * from autor where sifra between 1 and 16000;
 
+#odaberi sve autore koji su rođeni u 9. mjesecu
 select * from autor where datumrodenja like '%%%%-09-%%';
+
+select * from katalog where sifra between 1 and 3000;

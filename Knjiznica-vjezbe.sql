75aae11f25d2557f1b702120ccfc007110fccff0
Josip-github <josipcota1@gmail.com>
2021-05-19 15:37:56 +0200

    select, like
    Otkrij tko je autor knjige Lara Croft!
    Dva su autora: Fina Peler i Brigita Flis

--- Knjiznica-vjezbe.sql
+++ Knjiznica-vjezbe.sql
@@ -4,3 +4,9 @@ select * from autor where sifra between 1 and 16000;
 select * from autor where datumrodenja like '%%%%-09-%%';
 
 select * from katalog where sifra between 1 and 3000;
+
+#otrkij tko je autor knjige Lara Croft
+select * from katalog where naslov like 'Lara%';
+
+select * from autor where sifra = 18153;
+select * from autor where sifra = 19058;
